897add757d6a82d8cb35b20be98bfa95b680b115
Dees_Troy <dees_troy@teamw.in>
2012-10-24 19:58:10 +0000

    Fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,8 @@
 TARGET_OTA_ASSERT_DEVICE := d2mtr
 
 # Kernel
-TARGET_KERNEL_CONFIG    := cyanogen_d2mtr_defconfig
+#TARGET_KERNEL_CONFIG    := cyanogen_d2mtr_defconfig
+TARGET_PREBUILT_KERNEL      := device/samsung/d2mtr/kernel
 
 
 #TWRP
